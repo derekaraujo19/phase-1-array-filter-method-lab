e76363cd1acef3ea89ec11386a1c1a1bbb06c5ac
Derek Araujo <derekaraujo19@gmail.com>
2021-08-23 14:32:43 -0400

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+function findMatching(drivers, string) {
+  let matchList = drivers.filter(function(driver) {
+    return driver.toUpperCase() === string.toUpperCase();
+  })
+  return matchList;
+}
+
+
+function fuzzyMatch(drivers, string) {
+  let matchList = drivers.filter(function(driver) {
+    if (string.length === 2) {
+      return (driver.slice(0, 2) === string);
+    } else {
+      return (driver[0] === string);
+    }
+  })
+  return matchList;
+}
+
+function matchName(drivers, string) {
+  let matchList = drivers.filter(function(element) {
+    return element.name === string;
+  })
+  return matchList;
+}
